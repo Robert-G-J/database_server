1a3ff52ddafaf2e670ddb62e8a836ee97be52b04
Robert Jones <robert.g.jones@gmail.com>
2017-07-25 20:12:04 +0100

    Server responds with nil-string to nil-string GET
    This requires only a change of the GET helper method, such that it has a
    default argument of nil-string when no argument is supplied. The code
    otherwise already meets the condition.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -14,23 +14,30 @@ RSpec.describe 'DatabaseServer' do
   let(:value) { "somevalue" }
   let(:post_data) { { key => value } }
 
-  describe 'GET requests' do
-    it 'return a hardcoded value from memory' do
-      make_post_request(post_data) 
-      make_get_request(key)
-      expect(last_response.body).to eq value
-    end
-  end
-
   describe 'POST requests' do
     it 'stores a key-value pair' do
       make_post_request(post_data)
       expect(last_response.body).to eq(post_data.flatten.to_s)
     end
   end
-#-------- Helpers ----------------
 
-  def make_get_request(key)
+  describe 'GET requests' do
+    it 'return a hardcoded value from memory' do
+      make_post_request(post_data) 
+      make_get_request(key)
+      expect(last_response.body).to eq value
+    end
+
+    it 'return nil-string from nil-string key' do
+      make_post_request(post_data)
+      make_get_request()
+      expect(last_response.body).to eq nil.to_s
+    end
+  end
+
+  #-------- Helpers ----------------
+
+  def make_get_request(key = nil.to_s)
     get "/get?key=#{key}"
   end
 
