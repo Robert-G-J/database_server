aa90c1ebfc0f313836b8150103947617db8ff1e7
Robert Jones <robert.g.jones@gmail.com>
2017-07-25 20:32:00 +0100

    Refactor tests
    This refactor introduces 'context' to better organise and describe the
    tests. Some wording is changed to improve clarity.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -15,29 +15,33 @@ RSpec.describe 'DatabaseServer' do
   let(:post_data) { { key => value } }
 
   describe 'POST requests' do
-    it 'stores a key-value pair' do
+    it 'store a key-value pair in memory' do
       make_post_request(post_data)
       expect(last_response.body).to eq(post_data.flatten.to_s)
     end
   end
 
-  describe 'GET requests' do
-    it 'return a hardcoded value from memory' do
+  describe 'Making a POST, then a GET request' do
+    it 'stores, then returns a hardcoded value from memory' do
       make_post_request(post_data) 
       make_get_request(key)
       expect(last_response.body).to eq value
     end
 
-    it 'return nil-string from nil-string key' do
-      make_post_request(post_data)
-      make_get_request()
-      expect(last_response.body).to eq nil.to_s
+    context 'with a nil-string key' do
+      it 'return nil-string' do
+        make_post_request(post_data)
+        make_get_request()
+        expect(last_response.body).to eq nil.to_s
+      end
     end
 
-    it 'return nil-string for unknown key' do
-      make_post_request(post_data)
-      make_get_request('unknown key')
-      expect(last_response.body).to eq nil.to_s
+    context 'with an unknown key' do
+      it 'return nil-string for unknown key' do
+        make_post_request(post_data)
+        make_get_request('unknown key')
+        expect(last_response.body).to eq nil.to_s
+      end
     end
   end
 
@@ -50,4 +54,5 @@ RSpec.describe 'DatabaseServer' do
   def make_post_request(post_data)
     post 'set', post_data
   end
+  
 end
