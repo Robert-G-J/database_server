dfbf2d835743aafe8a726bab2550bca550f6eead
Robert Jones <robert.g.jones@gmail.com>
2017-07-25 19:55:09 +0100

    Refactor tests
    The tests were hardcoded, but now have helper methods for getting and
    posting data to aid readability.
    Note, however, that the 'get request' test remains trivial, as the value
    it returns is hardcoded. This will be altered by posting data first, so
    that there might be data that can be retrieved.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -10,17 +10,27 @@ RSpec.describe 'DatabaseServer' do
 
 #----------------------------------  
 
+  let(:key) { "somekey" }
+  let(:value) { "somevalue" }
+  let(:post_data) { { key => value } }
+
   describe 'GET requests' do
     it 'return a hardcoded value from memory' do
-      get '/get?key=somekey'
-      expect(last_response.body).to eq 'somevalue'
+      make_get_request(key)
+      expect(last_response.body).to eq value
     end
   end
 
   describe 'POST requests' do
     it 'stores a key-value pair' do
-      post 'set', 'somekey' => 'somevalue'
-      expect(last_response.body).to eq("[\"somekey\", \"somevalue\"]")
+      post 'set', post_data
+      expect(last_response.body).to eq(post_data.flatten.to_s)
     end
   end
+#-------- Helpers ----------------
+
+  def make_get_request(key)
+    get "/get?key=#{key}"
+  end
+
 end
